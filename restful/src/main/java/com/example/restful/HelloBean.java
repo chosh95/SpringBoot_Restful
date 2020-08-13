188ed51605fb375c71f962dac46dcaeea90359c8
chosh95 <chosh-95@hanmail.net>
2020-08-13 20:48:45 +0900

    hello controller

---
+++ restful/src/main/java/com/example/restful/HelloBean.java
@@ -0,0 +1,12 @@
+package com.example.restful;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloBean {
+
+    private String message;
+
+}
---
+++ restful/src/main/java/com/example/restful/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.restful;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @GetMapping("/hello-bean")
+    public HelloBean helloBean(){
+        return new HelloBean("Hello World");
+    }
+
+    @GetMapping("/hello-bean/{name}")
+    public HelloBean helloBean(@PathVariable("name") String name){
+        return new HelloBean(name);
+    }
+}
